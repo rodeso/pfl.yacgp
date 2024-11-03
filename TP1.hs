7639498745de1e9de8097f6a5024c3dd600d969d
Leonor2004 <up202205796@up.pt>
2024-11-03 11:44:21 +0000

    isStronglyConnected done
    Co-authored-by: Rodrigo de Sousa <rodeso@users.noreply.github.com>

--- TP1.hs
+++ TP1.hs
@@ -1,6 +1,6 @@
 import qualified Data.List
---import qualified Data.Array
---import qualified Data.Bits
+import qualified Data.Array
+import qualified Data.Bits
 
 -- PFL 2024/2025 Practical assignment 1
 
@@ -20,7 +20,7 @@ type RoadMap = [(City,City,Distance)]
 
 ---
 
--- 1. cities returns all the cities in the graph.
+-- 1. cities returns all the cities in the graph
 cities :: RoadMap -> [City]
 cities road = eliminateDuplicates (citiesAux road)
 
@@ -47,7 +47,7 @@ areAdjacentAux ((a,b,_):others) city1 city2 =
 
 ---
 
--- 3. distance returns a Just value with the distance between two cities connected directly, given two city names, and Nothing otherwise.
+-- 3. distance returns a Just value with the distance between two cities connected directly, given two city names, and Nothing otherwise
 distance :: RoadMap -> City -> City -> Maybe Distance
 distance road city1 city2 =
     case (distanceAux road city1 city2, distanceAux road city2 city1) of
@@ -63,7 +63,7 @@ distanceAux ((a,b,dance):others) city1 city2 =
 
 ---
 
--- 4. adjacent returns the cities adjacent to a particular city (i.e. cities with a direct edge between them) and the respective distances to them.
+-- 4. adjacent returns the cities adjacent to a particular city (i.e. cities with a direct edge between them) and the respective distances to them
 adjacent :: RoadMap -> City -> [(City,Distance)]
 adjacent road city = adjacentAuxA road city ++ adjacentAuxB road city
 
@@ -81,7 +81,7 @@ adjacentAuxB ((a, b , d):others) city =
 
 ---
 
--- 5. pathDistance returns the sum of all individual distances in a path between two cities in a Just value, if all the consecutive pairs of cities are directly connected by roads. Otherwise, it returns a Nothing.
+-- 5. pathDistance returns the sum of all individual distances in a path between two cities in a Just value, if all the consecutive pairs of cities are directly connected by roads. Otherwise, it returns a Nothing
 
 pathDistance :: RoadMap -> Path -> Maybe Distance
 pathDistance _ [] = Just 0
@@ -97,6 +97,7 @@ calculateTotalDistance road ((a, b):xs) = do
 
 ---
 
+-- rome returns the names of the cities with the highest number of roads connecting to them (i.e. the vertices with the highest degree)
 rome :: RoadMap -> [City]
 rome roadMap =
   let
@@ -114,16 +115,45 @@ romeAux roadMap =
 
 ---
 
+-- isStronglyConnected :: RoadMap -> Bool, returns a boolean indicat-ing whether all the cities in the graph are connected in the roadmap (i.e., if every city is reachable from every other city)
 isStronglyConnected :: RoadMap -> Bool
-isStronglyConnected = undefined
+isStronglyConnected roadMap = 
+    let 
+        allCities = cities roadMap
+        startCity = head allCities
+        visitedCities = bfs roadMap startCity
+    in 
+        length (eliminateDuplicates visitedCities) == length allCities
+
+
+bfs :: RoadMap -> City -> [City]
+bfs roadMap start = bfs' [start] []
+    where
+        bfs' :: [City] -> [City] -> [City]
+        bfs' [] visited = visited
+        bfs' (currentCity:queue) visited
+            | currentCity `elem` visited = bfs' queue visited
+            | otherwise =
+                let 
+                    adj = [nextCity | (nextCity, _) <- adjacent roadMap currentCity]
+                in 
+                    bfs' (queue ++ adj) (currentCity : visited)
+            
+
 
 ---
 
+-- shortestPath computes all shortest paths [RL99, BG20] connecting the two cities given as input.
+-- Note that there may be more than one path with the same total distance. If there are no paths between the input cities, then return an empty list. Note that the (only) shortest path between a city c and itself is [c].
 shortestPath :: RoadMap -> City -> City -> [Path]
 shortestPath = undefined
 
 ---
 
+-- travelSales :: RoadMap -> Path, given a roadmap, returns a solution of the Traveling Salesman Problem (TSP). In this problem, a traveling salesperson has to visit each city exactly once and come back to the start-
+-- ing town. The problem is to find the shortest route, that is, the route whose total distance is minimum. This problem has a known solution using dynamic programming [RL99]. Any optimal TSP path will be ac-
+-- cepted and the function only needs to return one of them, so the starting city (which is also the ending city) is left to be chosen by each group. Note that the roadmap might not be a complete graph (i.e. a graph where all
+-- vertices are connected to all other vertices). If the graph does not have a TSP path, then return an empty list.
 travelSales :: RoadMap -> Path
 travelSales = undefined
 
@@ -143,3 +173,30 @@ gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2",
 
 gTest3 :: RoadMap -- unconnected graph
 gTest3 = [("0","1",4),("2","3",2)]
+
+-- Fully connected small graph
+gTest4 :: RoadMap
+gTest4 = [("A", "B", 5), ("A", "C", 10), ("B", "C", 3), ("B", "D", 8), ("C", "D", 2)]
+
+-- Disconnected graph with two separate components
+gTest5 :: RoadMap
+gTest5 = [("A", "B", 7), ("B", "C", 4), ("D", "E", 6)]
+
+-- Circular route where each city is connected in a loop
+gTest6 :: RoadMap
+gTest6 = [("A", "B", 1), ("B", "C", 1), ("C", "D", 1), ("D", "A", 1)]
+
+-- Single city with no roads
+gTest7 :: RoadMap
+gTest7 = [("A", "A", 0)]  -- Or simply [], representing a single city with no connections
+
+-- Tree structure (connected but with no cycles)
+gTest8 :: RoadMap
+gTest8 = [("A", "B", 3), ("A", "C", 4), ("B", "D", 2), ("B", "E", 1), ("C", "F", 6), ("C", "G", 5)]
+
+-- Star structure (one central city connected to all others)
+gTest9 :: RoadMap
+gTest9 = [("Center", "A", 2), ("Center", "B", 4), ("Center", "C", 6), ("Center", "D", 8)]
+
+gTest10 :: RoadMap 
+gTest10 = [("0", "1", 10), ("0", "2", 15), ("0", "3", 20), ("1", "2", 35), ("1", "3", 25)]
